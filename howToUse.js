5d5229f2ebff8da2a32e8f2de8df372f9d303170
garrettjoecox <garrettjcox@gmail.com>
2015-05-22 00:08:51 -0700

    (Refactor) Redone logic and the way it works again..

--- howToUse.js
+++ howToUse.js
@@ -1,33 +1,14 @@
 
-// To declare a logger
 var log = require('./loggerlite');
 
-// Simply use it where you would normally console.log
-log('Hello World!'); // [Logger]: Hello World!
+var person = 'Garrett Cox';
+var color = 'Orange';
+var age = 19;
+var friend = 'Blake Mckinley';
+var weather = 'Sunny';
 
-// I've also made integrated lodash's templating, which looks
-// a lot cleaner than doing a bunch of +'s! Use ${a} through ${d}
-var greeting = 'Hello';
-var name = 'Garrett Cox';
-log('${a}, ${b}', greeting, name); // [Logger]: Hello, Garrett Cox
+log('Hello %s, today\'s forcast is %s.', person, weather);
 
-// What if you hate blue and magenta? Lucky for you loggerlite is
-// fully customizable, you can make themes and use them accordingly.
-// You simply invoke the require statement passing in an options Object
+log.err('There is a problem, %s should\'nt be %s already!', person, age);
 
-var options = {
-  bracketType: '{}',
-  bracketColor: 'grey',
-  prefix: ' Error ',
-  prefixColor: 'red',
-  seperator: '->',
-  seperatorColor: 'white',
-  textColor: 'red'
-};
-
-var errlog = require('./loggerlite')(options);
-
-var error = 'This thing broke here!';
-errlog('Something went wrong: ${a}', error);
-
-log('sup');
+log.ok('%s\'s favorite color is %s.', friend, color);
--- loggerlite.js
+++ loggerlite.js
@@ -1,31 +1,29 @@
 
-var template = require('lodash/string/template');
 require('colorslite');
 
-var defaults = {
-  bracketType: '[]',
-  bracketColor: 'blue',
-  prefix: 'Logger',
-  prefixColor: 'magenta',
-  seperator: ':',
-  seperatorColor: 'white',
-  textColor: 'grey',
+var options = {
+  brackets: '()',
+  seperator: ': ',
+  main: 'logger'.blue,
+  ok: 'ok'.green,
+  err: 'err'.red
 };
 
-module.exports = function(arg1, a1, b1, c1, d1){
-  if (typeof arg1 !== 'object'){
-    var prefix = defaults.bracketType[0][defaults.bracketColor] + defaults.prefix[defaults.prefixColor] + defaults.bracketType[1][defaults.bracketColor] + defaults.seperator[defaults.seperatorColor] + ' ';
-    var log = template(arg1)({ a:a1, b:b1, c:c1, d:d1 })[defaults.textColor];
-    console.log(prefix + log);
-  } else {
-    var newOptions = Object.create(defaults);
-    for (var key in arg1){
-      newOptions[key] = arg1[key];
-    }
-    return function(arg2, a2, b2, c2, d2){
-      var prefix = newOptions.bracketType[0][newOptions.bracketColor] + newOptions.prefix[newOptions.prefixColor] + newOptions.bracketType[1][newOptions.bracketColor] + newOptions.seperator[newOptions.seperatorColor] + ' ';
-      var log = template(arg2)({ a:a2, b:b2, c:c2, d:d2 })[newOptions.textColor];
-      console.log(prefix + log);
-    };
-  }
+var main = options.brackets[0] + options.main + options.brackets[1];
+var err = options.brackets[0] + options.err + options.brackets[1];
+var ok = options.brackets[0] + options.ok + options.brackets[1];
+
+module.exports = function() {
+  arguments[0] = main + options.seperator + arguments[0];
+  console.log.apply(this, arguments);
+};
+
+module.exports.ok = function() {
+  arguments[0] = main + ok + options.seperator + arguments[0].green;
+  console.log.apply(this, arguments);
+};
+
+module.exports.err = function() {
+  arguments[0] = main + err + options.seperator + arguments[0].red;
+  console.log.apply(this, arguments);
 };
